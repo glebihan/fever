2037d0ba5d363bc2834717b6e50adf6d74c5a877
Gwendal Le Bihan <gwendal.lebihan.dev@gmail.com>
2014-01-07 07:47:06 +0100

    Added license information to all files

--- Fever/FeverAccount.py
+++ Fever/FeverAccount.py
@@ -1,5 +1,22 @@
-#! /usr/bin/python
-# -*- coding=utf-8 -*-
+# FeverAccount.py
+# -*- Mode: Python; indent-tabs-mode: nil; tab-width: 4; coding: utf-8 -*-
+#
+# Copyright © 2013-2014 Gwendal Le Bihan
+# 
+# This file is part of Fever.
+# 
+# Fever is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Fever is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Fever.  If not, see <http://www.gnu.org/licenses/>.
 
 from EventsObject import EventsObject
 from ThreadedTask import ThreadedTask
